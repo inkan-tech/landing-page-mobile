d5e24a9f859331e11c30194f0592c0ab64c845fc
thomnico <thomnico@gmail.com>
2022-08-08 22:12:36 +0200

    Find a way to build with Grunt the same site than with bootstrap

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,168 @@
-module.exports = function(grunt) {
-    grunt.loadNpmTasks('grunt-pug-i18n');
-grunt.initConfig({
+const sass = require('node-sass');
+module.exports = grunt => {
+
+  grunt.initConfig({
+    // pug task src: https://github.com/yussan/grunt-pug-sass-skeleton/
     pug: {
-      templates: {
+      compile: {
         options: {
-          // Pug i18n specific options
-          i18n: {
-            locales: 'locales/*.json',
-            namespace: '$i18n'
+          data: {
+            debug: true
           },
-          // Pug specific options
-          pretty: true,
-          localeExtension: true,
+          pretty: true
         },
+        files: [{
+          src: '[^_]*.pug',
+          cwd: 'src/pug/',
+          dest: 'dist',
+          expand: true,
+          ext: '.html'
+        }]
+      }
+    },
+
+    sass: { // Task
+        dist: {
+            options: {
+                      style: 'inline',
+                      implementation: sass,
+                     },
+          files: [{
+              // Set to true for recursive search
+              expand: true,
+              cwd: 'scss/',
+              src: ['**/*.scss'],
+              dest: 'css/',
+              ext: '.css'
+          }]
+        }
+     },
+    //    // sass task
+    // sass: {
+    //   dist: {
+    //     options: {
+    //       style: 'inline',
+    //       implementation: scss,
+    //      },
+    //     // files: [{
+    //     //   src: '[^_]*.sass',
+    //     //   cwd: 'src/scss/',
+    //     //   dest: 'dist/css',
+    //     //   expand: true,
+    //     //   ext: '.css'
+    //     // }]
+    //     files: { "dist/css/styles.css" : [ "node_modules/bootstrap/scss/bootstrap.scss", "src/scss/*.scss"]}
+    //   }
+    // },
+
+    // copy task (copy src/libraries to dist/libraries)
+    copy: {
+      library: {
+        expand: true,
+        cwd: 'src',
+        src: ['assets/**'],
+        dest: 'dist/'
+      }
+    },
+
+    // image compress task (compress all image src/images to dist/images)
+    imagemin : {
+      dynamic: {
+          files: [{
+              expand: true,
+              cwd: 'src/',
+              src: ['assets/**/*.{png,jpg,gif}'],
+              dest: 'dist'
+          }]
+      }
+    },
+
+    //minify css (only tun in production)
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'dist/css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'dist/css',
+          ext: '.css'
+        }]
+      }
+    },
+
+    // auto refresh view on change in dist directory
+    browserSync: {
+      dev: {
+        bsFiles: {
+            src : [
+                'dist/**.*'
+            ]
+        },
+        options: {
+            watchTask: true,
+            server: './dist'
+        }
+      }
+    },
+
+    // watch change inside directory to run task
+    watch: {
+      pug: {
+        files: ['src/pug/**/*.pug'],
+        tasks: ['pug']
+      },
+      sass: {
+        files: ['src/scss/**/*.scss'],
+        tasks: ['sass']
+      },
+      js: {
+        files: ['src/js/**/*.js'],
+        tasks: ['babel']
+      },
+      babel: {
+         files: ['src/js/**/*.js'],
+         tasks: ['babel']
+       },
+      copy: {
+        files: ['src/assets/**'],
+        tasks: ['copy']
+      },
+      imagemin: {
+        files: ['src/assets/**'],
+        tasks: ['imagemin']
+      }
+    },
+
+    // babel
+    babel: {
+      options: {
+        sourceMap: false
+      },
+      dist: {
         files: {
-          "html": ["src/pug/*.pug", "src/pug/includes/*.pug"]
+          'dist/js/scripts.js': 'src/js/scripts.js'
         }
       }
     }
   });
-}
+
+  // initial
+  grunt.loadNpmTasks('grunt-contrib-pug');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-babel');
+
+  //register default task
+  if(process.env.NODE_ENV == 'production')
+  {
+    grunt.registerTask('default', ['pug', 'sass', 'copy', 'imagemin', 'cssmin', 'babel'])
+  }else
+  {
+    grunt.registerTask('default', ['pug', 'sass', 'copy', 'imagemin', 'browserSync', 'babel', 'watch'])
+  }
+};
+
