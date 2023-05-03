d037ec05a45a381728b71c4b098cada22c80af59
thomnico <thomnico@gmail.com>
2023-05-03 21:42:41 +0200

    Attempt matomo integ in js/scripts.js

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -24,3 +24,34 @@ window.addEventListener('DOMContentLoaded', event => {
     });
   });
 });
+
+//
+// Matomo scripts
+//
+
+var _paq = window._paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function () {
+  var u = "https://sealfie.matomo.cloud/";
+  _paq.push(['setTrackerUrl', u + 'matomo.php']);
+  _paq.push(['setSiteId', '1']);
+  var d = document,
+    g = d.createElement('script'),
+    s = d.getElementsByTagName('script')[0];
+  g.async = true;
+  g.src = '//cdn.matomo.cloud/sealfie.matomo.cloud/matomo.js';
+  s.parentNode.insertBefore(g, s);
+})();
+var _mtm = window._mtm = window._mtm || [];
+_mtm.push({
+  'mtm.startTime': new Date().getTime(),
+  'event': 'mtm.Start'
+});
+var d = document,
+  g = d.createElement('script'),
+  s = d.getElementsByTagName('script')[0];
+g.async = true;
+g.src = 'https://cdn.matomo.cloud/sealfie.matomo.cloud/container_5gw1cTb8.js';
+s.parentNode.insertBefore(g, s);
