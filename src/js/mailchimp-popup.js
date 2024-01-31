77f266a179f4a8f6a3e538e7c8f5617645621e28
thomnico <thomnico@gmail.com>
2024-01-31 10:58:30 +0100

    upgrades make to register button go to fixed registration page, keep the popup fonction

--- src/js/mailchimp-popup.js
+++ src/js/mailchimp-popup.js
@@ -1,13 +1,15 @@
 !function (c, h, i, m, p) { m = c.createElement(h), p = c.getElementsByTagName(h)[0], m.async = 1, m.src = i, p.parentNode.insertBefore(m, p) }(document, "script", "https://chimpstatic.com/mcjs-connected/js/users/716e8fcfd71b495b7e4af1e92/a9a7638a80da74b7afc8254dd.js");
 
-function showPopup() {
-     //unsetting the cookie
+function showMailChimpPopup() {
+
+    !function (c, h, i, m, p) { m = c.createElement(h), p = c.getElementsByTagName(h)[0], m.async = 1, m.src = i, p.parentNode.insertBefore(m, p) }(document, "script", "https://chimpstatic.com/mcjs-connected/js/users/716e8fcfd71b495b7e4af1e92/a9a7638a80da74b7afc8254dd.js");
+    //unsetting the cookie
     document.cookie = "MCPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     document.cookie = "MCPopupSubscribed=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    !function (c, h, i, m, p) { m = c.createElement(h), p = c.getElementsByTagName(h)[0], m.async = 1, m.src = i, p.parentNode.insertBefore(m, p) }(document, "script", "https://chimpstatic.com/mcjs-connected/js/users/716e8fcfd71b495b7e4af1e92/a9a7638a80da74b7afc8254dd.js");
-
+    // Refresh the page and bypass the cache
+    location.reload(true);
     // scroll down as the popup is triggered by that.
     window.scrollTo(0, 1500);
 }
 
-document.getElementById("show-popup").onclick = function () { showPopup(); }
+document.getElementById("show-popup").onclick = function () { showMailChimpPopup(); }
